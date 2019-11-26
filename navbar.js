d45ba73dfd45fdae28a8960966381519561448d8
Hinthujan <hinthu5@hotmail.no>
2019-11-26 21:55:38 +0100

    made hambar

--- navbar.js
+++
@@ -1,7 +0,0 @@
-// function selection
-const selectElement = (element) => document.querySelector(element);
-selectElement('.hamburger').addEventListener('click', () =>{
-    selectElement('.hamburger').classList.toggle('active');
-    selectElement('.nav-list').classList.toggle('active');
-    selectElement('.mainNav').classList.toggle('active');
-});
