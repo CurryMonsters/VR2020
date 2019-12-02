04f6ff89689f2b329c0f023491aeef9cae5a6021
Hinthujan <hinthu5@hotmail.no>
2019-12-02 18:33:47 +0100

    made changes to the menu

--- fullscreen.js
+++ fullscreen.js
@@ -2,3 +2,13 @@ const selectElement = (element) => document.querySelector(element);
 selectElement('.toggler').addEventListener('click', () =>{
     selectElement('html').classList.toggle('full-screen');
 });
+
+selectElement('.close-on-click').addEventListener('click', () =>{
+    document.getElementById("tickbox").checked = false;
+    selectElement('html').classList.remove('full-screen');
+});
+
+selectElement('.close-on-clicked').addEventListener('click', () =>{
+    document.getElementById("tickbox").checked = false;
+    selectElement('html').classList.remove('full-screen');
+});
