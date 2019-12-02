7cf20af7e2c75b5d184de69138e5af3cc72a840f
Hinthujan <hinthu5@hotmail.no>
2019-12-02 18:37:38 +0100

    g

--- fullscreen.js
+++ fullscreen.js
@@ -3,12 +3,12 @@ selectElement('.toggler').addEventListener('click', () =>{
     selectElement('html').classList.toggle('full-screen');
 });
 
-selectElement('.close-on-click').addEventListener('click', () =>{
-    document.getElementById("tickbox").checked = false;
-    selectElement('html').classList.remove('full-screen');
-});
+// selectElement('.close-on-click').addEventListener('click', () =>{
+//     document.getElementById("tickbox").checked = false;
+//     selectElement('html').classList.remove('full-screen');
+// });
 
-selectElement('.close-on-clicked').addEventListener('click', () =>{
-    document.getElementById("tickbox").checked = false;
-    selectElement('html').classList.remove('full-screen');
-});
+// selectElement('.close-on-clicked').addEventListener('click', () =>{
+//     document.getElementById("tickbox").checked = false;
+//     selectElement('html').classList.remove('full-screen');
+// });
